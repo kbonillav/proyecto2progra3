544b9d25c5572f4e681c6ea78faa5b94acd8eb85
kbonillav <109880734+kbonillav@users.noreply.github.com>
2024-03-25 20:42:35 -0600

    Add files via upload

---
+++ Categoria_ArticuloAD.cs
@@ -0,0 +1,29 @@
+﻿using Entidades;
+
+namespace AccesoDatos
+{
+    public static class Categoria_ArticuloAD
+    {
+        private static Categoria_Articulo[] categorias = new Categoria_Articulo[20];
+        private static int indicecategoria = 0;
+
+        public static bool GuardarCategoria(Categoria_Articulo categoria)
+        {
+            try
+            {
+                categorias[indicecategoria] = categoria;
+                indicecategoria++;
+                return true;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new IndexOutOfRangeException("El repositorio esta lleno y no permite registrar más información");
+            }
+        }
+
+        public static Categoria_Articulo[] ConsultarCategorias() 
+        { 
+            return categorias;
+        }
+    }
+}
